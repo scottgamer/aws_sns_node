4ac9c3b3c24e4fe0544e52c20a7d4e759e3b447f
Richard Munoz <richosojason@msn.com>
2019-09-12 16:59:32 -0500

    added get topic attributes

---
+++ sns_gettopicattributes.js
@@ -0,0 +1,16 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set region
+AWS.config.update({ region: 'REGION' });
+
+// Create promise and SNS service object
+const getTopicAttribsPromise = new AWS.SNS({ apiVersion: '2010-03-31' })
+  .getTopicAttributes({ TopicArn: 'TOPIC_ARN' })
+  .promise();
+
+// Handle promise's fulfilled/rejected states
+getTopicAttribsPromise
+  .then(data =>
+    console.log(data))
+  .catch(err =>
+    console.error(err, err.stack));
