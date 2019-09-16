99f0e7a5df685bc5d0045c43b60c6153b3db70df
Richard Munoz <richosojason@msn.com>
2019-09-16 09:13:02 -0500

    babel added

--- build/app.js
+++ build/app.js
@@ -1,8 +1,12 @@
 'use strict';
 
-var express = require('express');
+var _express = require('express');
 
-var app = express();
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 var PORT = process.env.port || 3000;
 
 app.listen(PORT, function () {
